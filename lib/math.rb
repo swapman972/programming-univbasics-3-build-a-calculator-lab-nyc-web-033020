2221bbaf7ded358a63d8c457bacfe553637636fc
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-20 19:15:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,12 +4,12 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  subtr = num2 - num1
+  subtr = num1 - num2
   subtr
 end
 
 def division(num1, num2)
-  div = (num2)/(num1)
+  div = (num1)/(num2)
   div
 end
 
@@ -19,7 +19,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  mod = num2 % num1
+  mod = num1 % num2
   mod
 end
 
