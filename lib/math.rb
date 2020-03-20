d265290fa791e3a9739b3c2d9048449bfa4abd0e
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-20 19:16:11 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  add
 end
 
 def subtraction(num1, num2)
-
+  subtr = num1 - num2
+  subtr
 end
 
 def division(num1, num2)
-
+  div = (num1)/(num2)
+  div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
-
+  squar = Math.sqrt(num)
+  squar
 end
